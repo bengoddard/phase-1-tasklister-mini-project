eb38009f5304b5448db94317228e9e7c6eed7d33
Ben Goddard <goddard.ben04@gmail.com>
2025-10-22 17:43:56 -0600

    Created to do list functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+//const { createElement } = require("react");
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      const formInput = document.querySelector("#new-task-description");
+      const task = formInput.value.trim();
+      console.log(formInput);
+      console.log(task);
+      buildToDo(task);
+    })
+
+  function buildToDo(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    const taskList = document.querySelector("#tasks");
+    taskList.append(li);
+  }
 });
